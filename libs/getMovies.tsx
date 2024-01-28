ea22136792468874e4e90e93eb3ed17a96d92282
chumchantha <101447078+chumchantha@users.noreply.github.com>
2024-01-28 16:58:56 +0700

    store cache

--- libs/getMovies.tsx
+++ libs/getMovies.tsx
@@ -1,8 +1,8 @@
 const getMovies = async () => {
   const res = await fetch(
     // "http://127.0.0.1:8090/api/collections/movies/records?sort=@random",
-    "https://soydrama.pockethost.io/api/collections/movies/records?sort=@random"
-    // { cache: "no-store" }
+    "https://soydrama.pockethost.io/api/collections/movies/records?sort=@random",
+    { cache: "no-store" }
   );
   const data = await res.json();
 
